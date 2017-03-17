e2ba8469e47b7afc00a3d61321b912227e1454d4
Nazar4i <karpynnv@gmail.com>
2017-03-17 14:15:11 +0200

    Update AppConfig.java

--- src/main/java/com/home/task/config/AppConfig.java
+++ src/main/java/com/home/task/config/AppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan("com.ua.kiev.makson")
+@ComponentScan("com.home.task")
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter {
 
